97b3c05b6cea769af43f378efec7f3fb8cc3318f
Zane Mingee <zmingee@outlook.com>
2017-09-25 11:39:31 -0400

    [DEL] Deleted unused dotfiles

--- fish/.config/fish/config.fish
+++
@@ -1,3 +0,0 @@
-set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Frameworks/Python.framework/Versions/3.4/bin /opt/X11/bin
-
-eval (python3 -m virtualfish)
--- fish/.config/fish/functions/fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_prompt
-	if set -q VIRTUAL_ENV
-        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-    end
-    set_color $fish_color_cwd
-	echo -n (basename $PWD)
-	set_color normal
-	echo -n ' ) '
-end
